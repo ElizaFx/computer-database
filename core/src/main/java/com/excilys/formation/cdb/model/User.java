6f2aa34976fa7b02adf3804804729f7043e108a9
Joxit <joxit972@gmail.com>
2015-05-25 11:43:07 +0200

    hashCode and equals for User and Authority

--- core/src/main/java/com/excilys/formation/cdb/model/User.java
+++ core/src/main/java/com/excilys/formation/cdb/model/User.java
@@ -28,7 +28,7 @@ public class User implements UserDetails, Serializable {
 	}
 
 	public void setEnable(boolean enable) {
-		this.enabled = enable;
+		enabled = enable;
 	}
 
 	public void setUsername(String username) {
@@ -82,6 +82,59 @@ public class User implements UserDetails, Serializable {
 	public String toString() {
 		return "User [username=" + username + ", password=" + password
 				+ ", enable=" + enabled + ", authorities=" + authorities + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result)
+				+ ((authorities == null) ? 0 : authorities.hashCode());
+		result = (prime * result) + (enabled ? 1231 : 1237);
+		result = (prime * result)
+				+ ((password == null) ? 0 : password.hashCode());
+		result = (prime * result)
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		User other = (User) obj;
+		if (authorities == null) {
+			if (other.authorities != null) {
+				return false;
+			}
+		} else if (!authorities.equals(other.authorities)) {
+			return false;
+		}
+		if (enabled != other.enabled) {
+			return false;
+		}
+		if (password == null) {
+			if (other.password != null) {
+				return false;
+			}
+		} else if (!password.equals(other.password)) {
+			return false;
+		}
+		if (username == null) {
+			if (other.username != null) {
+				return false;
+			}
+		} else if (!username.equals(other.username)) {
+			return false;
+		}
+		return true;
 	}
 
 }
